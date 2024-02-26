3bdbc341da061eb129a9c18bb4eafdd2a7271a77
Filmyrohesh <142705544+Filmyrohesh@users.noreply.github.com>
2024-02-26 14:40:34 +0530

    Update info.py

--- info.py
+++ info.py
@@ -74,7 +74,7 @@ if 'DYNO' in environ:
     APP_NAME = environ.get('APP_NAME')
 else:
     ON_HEROKU = False
-BIND_ADRESS = str(getenv('WEB_SERVER_BIND_ADDRESS', '0.0.0.0'))
+BIND_ADRESS = str(getenv('WEB_SERVER_BIND_ADDRESS', 'https://premiumfilterteat.onrender.com'))
 FQDN = str(getenv('FQDN', BIND_ADRESS)) if not ON_HEROKU or getenv('FQDN') else APP_NAME+'.herokuapp.com'
 URL = "https://premiumfilterteat.onrender.com/".format(FQDN) if ON_HEROKU or NO_PORT else \
     "https://premiumfilterteat.onrender.com/".format(FQDN, PORT)
