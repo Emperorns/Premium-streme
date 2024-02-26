d25f2be77579aa0e70fb955edddd36431c1b11ba
Nihal Singh <142614932+Emperorns@users.noreply.github.com>
2024-02-26 21:20:04 +0530

    Update info.py

--- info.py
+++ info.py
@@ -74,10 +74,10 @@ if 'DYNO' in environ:
     APP_NAME = environ.get('APP_NAME')
 else:
     ON_HEROKU = False
-BIND_ADRESS = str(environ.get('WEB_SERVER_BIND_ADDRESS', 'FQDN'))
-FQDN = str(environ.get('FQDN', BIND_ADRESS)) if not ON_HEROKU or environ.get('FQDN') else APP_NAME+'.herokuapp.com'
-URL = "https://premium-streme.onrender.com".format(FQDN) if ON_HEROKU or NO_PORT else \
-    "https://premium-streme.onrender.com".format(FQDN, PORT)
+BIND_ADRESS = str(getenv('WEB_SERVER_BIND_ADDRESS', '0.0.0.0'))
+FQDN = str(getenv('FQDN', BIND_ADRESS)) if not ON_HEROKU or getenv('FQDN') else APP_NAME+'.herokuapp.com'
+URL = "https://{}/".format(FQDN) if ON_HEROKU or NO_PORT else \
+    "https://{}/".format(FQDN, PORT)
 SLEEP_THRESHOLD = int(environ.get('SLEEP_THRESHOLD', '60'))
 WORKERS = int(environ.get('WORKERS', '4'))
 SESSION_NAME = str(environ.get('SESSION_NAME', 'LazyBot'))
@@ -86,15 +86,15 @@ name = str(environ.get('name', 'LazyPrincess'))
 PING_INTERVAL = int(environ.get("PING_INTERVAL", "1200"))  # 20 minutes
 if 'DYNO' in environ:
     ON_HEROKU = True
-    APP_NAME = str(environ.get('APP_NAME'))
+    APP_NAME = str(getenv('APP_NAME'))
 
 else:
     ON_HEROKU = False
-HAS_SSL=bool(environ.get('HAS_SSL',True))
+HAS_SSL=bool(getenv('HAS_SSL',False))
 if HAS_SSL:
-    URL = "https://premium-streme.onrender.com".format(FQDN)
+    URL = "https://{}/".format(FQDN)
 else:
-    URL = "https://premium-streme.onrender.com".format(FQDN)
+    URL = "https://{}/".format(FQDN)
     
 LOG_STR = "Current Cusomized Configurations are:-\n"
 LOG_STR += ("IMDB Results are enabled, Bot will be showing imdb details for you queries.\n" if IMDB else "IMBD Results are disabled.\n")
